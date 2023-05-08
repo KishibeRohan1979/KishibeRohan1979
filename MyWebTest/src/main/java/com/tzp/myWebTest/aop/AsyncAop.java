8741df2ca58413120428434e9063c49e40b5bbdf
康旭东 <kangxudong@piesat.cn>
2023-05-08 21:02:22 +0800

    ⚡aop层优化：可以不使用uuid了

--- MyWebTest/src/main/java/com/tzp/myWebTest/aop/AsyncAop.java
+++ MyWebTest/src/main/java/com/tzp/myWebTest/aop/AsyncAop.java
@@ -1,5 +1,6 @@
 package com.tzp.myWebTest.aop;
 
+import com.tzp.myWebTest.dto.MapCreateDTO;
 import com.tzp.myWebTest.service.AsyncService;
 import com.tzp.myWebTest.util.AsyncMsgUtil;
 import com.tzp.myWebTest.util.MsgUtil;
@@ -27,11 +28,31 @@ public class AsyncAop {
     }
 
     @Around("asyncPointCut()")
-    public MsgUtil around(ProceedingJoinPoint point) throws Throwable {
+    public MsgUtil<Object> around(ProceedingJoinPoint point) throws Throwable {
         //请求header
 //        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         //异步消息
-        String id = UUID.randomUUID().toString();
+//        String id = UUID.randomUUID().toString();
+        // 获取方法参数
+        Object[] args = point.getArgs();
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException("方法参数为空");
+        }
+        MapCreateDTO dto = null;
+        for (Object arg : args) {
+            if (arg instanceof MapCreateDTO) {
+                dto = (MapCreateDTO) arg;
+                break;
+            }
+        }
+        if (dto == null) {
+            throw new IllegalArgumentException("方法参数中未找到 MapCreateDTO 类型的参数");
+        }
+        // 获取 id
+        String id = dto.getOid();
+        if (id == null || id.isEmpty()) {
+            throw new IllegalArgumentException("MapCreateDTO 对象中的 oid 为空");
+        }
         AsyncMsgUtil asyncMsg = new AsyncMsgUtil();
         asyncMsg.setId(id);
         //异步执行

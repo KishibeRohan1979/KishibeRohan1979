fee7d5255fae85c648f42433bc2c4755412b7cd9
康旭东 <kangxudong@piesat.cn>
2023-05-09 00:40:42 +0800

    ⚡去掉咩用的import

--- MyWebTest/src/main/java/com/tzp/myWebTest/aop/AsyncAop.java
+++ MyWebTest/src/main/java/com/tzp/myWebTest/aop/AsyncAop.java
@@ -12,7 +12,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 @Aspect
